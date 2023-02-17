d89dfb58db2615b2bcc3b91d9d6855cda611b745
RedPull1997 <83898108+RedPull1997@users.noreply.github.com>
2023-02-17 11:12:33 +0900

    extends template

---
+++ src/features/template/Counter.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import styles from './Counter.module.css';
+
+export function Counter() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
